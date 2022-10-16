24b807e61b0138f6108762879dd00649739aee9a
SN1004 <ce20btech11041@iith.ac.in>
2022-10-16 15:39:17 +0530

    Level 2 Randomize at start
    Now level 2 start with randomized tiles instead of clicking play to randomize tile. Also the num of swaps are now the actual number that happens not num swaps - 1.

--- Project/Assets/scripts/TileMove.cs
+++ Project/Assets/scripts/TileMove.cs
@@ -8,16 +8,28 @@ public class TileMove : MonoBehaviour
     [SerializeField] private Transform emptySpace = null;
     [SerializeField] private int dist = 20;
     [SerializeField] Transform[] CurrentTilePos;
+    private static Vector3[] WinTilePos;
     [SerializeField] private int Num_Swaps = 21;
     private Camera Maincamera;
+    private static bool Inti = true;
     private void Start()
     {
+        WinTilePos = new Vector3[CurrentTilePos.Length];
+        Inti = true;
         Maincamera = Camera.main;
-        
+        for (int i = 0; i < CurrentTilePos.Length; i++) WinTilePos[i] = CurrentTilePos[i].position;
+        Play();
     }
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Inti == false) 
+            if (Winchk())
+            {
+                //Win = true
+                Debug.Log("win");
+            }
+
+                if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Maincamera.ScreenPointToRay(Input.mousePosition);
             RaycastHit2D Hit = Physics2D.Raycast(ray.origin, ray.direction);
@@ -33,6 +45,19 @@ public class TileMove : MonoBehaviour
         }
     }
 
+    private bool Winchk()
+    {
+        for (int i = 0; i < CurrentTilePos.Length; i++)
+        {
+            if(CurrentTilePos[i].position != WinTilePos[i])
+            {
+                // Win == false;
+                return false;
+            }
+        }
+        return true;
+    }
+
     private void Swap(Transform p1, Transform p2)
     {
         Vector2 posp1 = p1.position;
@@ -41,15 +66,21 @@ public class TileMove : MonoBehaviour
         p2.position = posp1;
     }
 
-    public void Play()
+    private void Play()
     {
         //randomize tiles
-        int n = Random.Range(0, Num_Swaps);
+        int n = Random.Range(0, Num_Swaps+1);
         for(int i = 0; i < n; i++)
         {
             Transform p1 = CurrentTilePos[Random.Range(0, 9)];
             Transform p2 = CurrentTilePos[Random.Range(0, 9)];
             Swap(p1,p2);
         }
+        if (Winchk())
+        {
+            Play();
+            Debug.Log("error in random");
+        }
+        else Inti = false;
     }
 }
