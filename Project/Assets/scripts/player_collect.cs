ea6f4b0a45830a48af6eb495f9aad8d1256e0fb7
JeevanIITH <cs20btech11047@iith.ac.in>
2022-11-07 15:51:03 +0530

    inventory

--- Project/Assets/scripts/player_collect.cs
+++ Project/Assets/scripts/player_collect.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class player_collect : MonoBehaviour
 {
+    public GameObject bone1, bone2, bone3, bone4, bone5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bone1.SetActive(false);
+        bone2.SetActive(false);
+        bone3.SetActive(false);
+        bone4.SetActive(false);
+        bone5.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -16,26 +21,32 @@ public class player_collect : MonoBehaviour
         {
             print("bone1");
             Destroy(collision.gameObject);
+            bone1.SetActive(true);
         }
         if (collision.gameObject.CompareTag("bone2"))
         {
             Destroy(collision.gameObject);
             print("bone1");
+            bone2.SetActive(true);
         }
         if (collision.gameObject.CompareTag("bone3"))
         {
+
             Destroy(collision.gameObject);
             print("bone1");
+            bone3.SetActive(true);
         }
         if (collision.gameObject.CompareTag("bone4"))
         {
             Destroy(collision.gameObject);
             print("bone1");
+            bone4.SetActive(true);
         }
         if (collision.gameObject.CompareTag("bone5"))
         {
             Destroy(collision.gameObject);
             print("bone1");
+            bone5.SetActive(true);
         }
     }
 
