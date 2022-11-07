0adee5547efe90008e0bdc2c691ac2e051eb207a
JeevanIITH <cs20btech11047@iith.ac.in>
2022-11-07 16:45:40 +0530

    time added

--- Project/Assets/scripts/player_collect.cs
+++ Project/Assets/scripts/player_collect.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.UI;
+using TMPro;
 
 public class player_collect : MonoBehaviour
 {
     public GameObject bone1, bone2, bone3, bone4, bone5;
+    public int no_collected = 0;
+    public bool collection_completed = false;
+    public TextMeshProUGUI tm;
+    public float timer = 0;
+    public int total_time = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,7 @@ public class player_collect : MonoBehaviour
         bone3.SetActive(false);
         bone4.SetActive(false);
         bone5.SetActive(false);
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -22,12 +30,14 @@ public class player_collect : MonoBehaviour
             print("bone1");
             Destroy(collision.gameObject);
             bone1.SetActive(true);
+            no_collected++;
         }
         if (collision.gameObject.CompareTag("bone2"))
         {
             Destroy(collision.gameObject);
             print("bone1");
             bone2.SetActive(true);
+            no_collected++;
         }
         if (collision.gameObject.CompareTag("bone3"))
         {
@@ -35,24 +45,33 @@ public class player_collect : MonoBehaviour
             Destroy(collision.gameObject);
             print("bone1");
             bone3.SetActive(true);
+            no_collected++;
         }
         if (collision.gameObject.CompareTag("bone4"))
         {
             Destroy(collision.gameObject);
             print("bone1");
             bone4.SetActive(true);
+            no_collected++;
         }
         if (collision.gameObject.CompareTag("bone5"))
         {
             Destroy(collision.gameObject);
             print("bone1");
             bone5.SetActive(true);
+            no_collected++;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (no_collected == 5)
+        {
+            collection_completed = true;
+            total_time = (int)timer;
+        }
+        timer += Time.deltaTime;
+        tm.text = (int)timer + "s";
     }
 }
