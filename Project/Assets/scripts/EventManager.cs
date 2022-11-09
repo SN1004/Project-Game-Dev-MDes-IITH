f8f9e429cf7e886d652518e3ae2103622752ea8b
SN1004 <jatinneekhra4@gmail.com>
2022-11-09 23:00:56 +0530

    Update all

--- Project/Assets/scripts/EventManager.cs
+++ Project/Assets/scripts/EventManager.cs
@@ -16,39 +16,59 @@ public class EventManager : MonoBehaviour
     [SerializeField] private Texture2D Cursor_normal;
     [SerializeField] private Vector2 Hotspot_normal;
     [SerializeField] private AudioSource Button_Sound;
+    
 
     // For camera movement
     /*    [SerializeField] Vector2 Max;     
         [SerializeField] Vector2 Min;*/
     [SerializeField] List<SceneAsset> OrderedScenes;
+    private static List<SceneAsset> OrderedSceneArray;
     private static int scenecount=0;
+    public static bool Level2Open = false;
+    private GameObject Level2SelectButton;
+
+    private void Update()
+    {
+        if(SceneManager.GetActiveScene().name == "level2")
+        {
+            Debug.Log("i am in");
+            Level2SelectButton = GameObject.FindGameObjectWithTag("GameCanvas");
+            Level2SelectButton.GetComponent<Button>().interactable = Level2Open;
+        }
+    }
+
+    public void Level1Select()
+    {
+        SceneManager.LoadScene("level1");
+    }
 
     public void Restart()
     {
         Button_Sound.Play();
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        /*DontDestroyOnLoad(this);
-        SceneManager.LoadScene(OrderedScenes[scenecount].name);*/
+        DontDestroyOnLoad(this);
     }
 
     public void Back()
     {
         Button_Sound.Play();
-        /*SceneManager.LoadScene("Level_Select");*/
+        SceneManager.LoadScene("Level_Select");
+        DontDestroyOnLoad(this);
     }
 
     public void Next()
     {
-        Button_Sound.Play();
-        /*  DontDestroyOnLoad(this);
+          Button_Sound.Play();
           scenecount += 1;
-          SceneManager.LoadScene(OrderedScenes[scenecount].name);*/
+          SceneManager.LoadScene(OrderedScenes[scenecount].name);
+          DontDestroyOnLoad(this);
     }
 
     public void Home()
     {
         Button_Sound.Play();
-        /*SceneManager.LoadScene("Home");*/
+        SceneManager.LoadScene("Home");
+        DontDestroyOnLoad(this);
     }
 
     public void Quit()
@@ -68,7 +88,7 @@ public class EventManager : MonoBehaviour
 
     public void MouseOnButtonExit()
     {
-    Cursor.SetCursor(Cursor_normal, Hotspot_normal, CursorMode.Auto);
+        Cursor.SetCursor(Cursor_normal, Hotspot_normal, CursorMode.Auto);
     }
 
     public static void WriteFile(string text)
